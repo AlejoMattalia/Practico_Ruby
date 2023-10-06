0dd0b34d5296596581965ffb6c56ee10503a8a59
Franco Pignatelli <francopignatelli15@gmail.com>
2023-10-06 11:35:04 -0300

    Agregados algunos comentarios, formateados los archivos, elimiminados metodos from_text y to_text de las clases Persona y Mascota, debido a que estos no eran utilizados

--- models/persona.rb
+++ models/persona.rb
@@ -20,29 +20,4 @@ class Persona
     @domicilio = domicilio
     @mascotas = mascotas.is_a?(Array) ? mascotas : []
   end
-
-  def to_text
-    TextController.serialize_to_text({
-      personaId: @personaId,
-      nombre: @nombre,
-      apellido: @apellido,
-      dni: @dni,
-      domicilio: @domicilio,
-      mascotas: @mascotas
-    })
-  end
-
-  def self.from_text(text)
-    data = TextController.deserialize_from_text(text)
-    Persona.new(
-      data[:personaId].to_i,
-      data[:nombre],
-      data[:apellido],
-      data[:dni].to_i,
-      data[:domicilio],
-      data[:mascotas]
-    )
-  end
-
-private
 end
